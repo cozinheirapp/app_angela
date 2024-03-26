0aa5df6e03ab7342a77a79778e3b410b03dc6f01
unknown <angela.dias@estudante.ifms.edu.br>
2024-03-25 21:02:01 -0400

    first commit

---
+++ ssc/pages/Listas.tsx
@@ -0,0 +1,11 @@
+import {Text, View} from "react-native";
+
+function listasScreen() {
+    return (
+        <View>
+            <Text>listasScreen</Text>
+        </View>
+    );
+}
+
+export default listasScreen;
